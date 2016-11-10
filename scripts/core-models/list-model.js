1b05865b5a8c26f639f3dea235b2b1c980d5ba09
ZahariyaPehlivanova <zahariqqq@abv.bg>
2016-11-10 17:01:44 +0200

    ImplementPunchStarterList

--- scripts/core-models/list-model.js
+++ scripts/core-models/list-model.js
@@ -3,8 +3,28 @@ class ListModel {
 
     }
 
-    render(){
+    render(database){
+        database.sort(function (a,b) {
+            return a.id - b.id;
+        });
 
+        let table = '';
+
+        for (let i = 0; i < database.length; i++) {
+            let current = database[i];
+            let per = (current.accumulatedMoney /current.targetPrice)*100;
+             table +=`<tr><td>${current.id}</td><td>${current.name}</td><td>${current.manufacturer}</td><td>${current.constructor.name.replace('PunchStarter','')}</td><td>${per.toFixed(2)}%</td></tr>\n`;
+        }
+
+        let header = `<tr><th>ID</th><th>Name</th><th>Manufacturer</th><th>Type</th><th>Progress</th></tr>`;
+        let html =            `
+<div class="punch-starter-list-holder">
+    <table class="punch-starter-table">
+        ${header}    
+        ${table}
+    </table>
+</div>`
+$('.wrapper main').html(html);
     }
 }
 
