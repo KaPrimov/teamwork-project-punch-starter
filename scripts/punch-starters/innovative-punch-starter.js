1e9c04806976e418f62d0a3899af83245ebbec3a
Zahariya <zahariyapehlivanova@gmail.com>
2016-11-10 15:40:04 +0200

    AddInnovativePunchStarter

--- scripts/punch-starters/innovative-punch-starter.js
+++ scripts/punch-starters/innovative-punch-starter.js
@@ -1,5 +1,8 @@
-class InnovativePunchStarter {
-	//TODO: Implement InnovativePunchStarter
+let BasePunchStarter = require('./base-punch-starter.js');
+class InnovativePunchStarter extends BasePunchStarter{
+    constructor(id, name, manufacturer, description, genres, targetPrice) {
+        super(id, name, manufacturer, description, genres, targetPrice);
+    }
 }
 
 module.exports = InnovativePunchStarter;
